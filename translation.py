dff37d80cd607e4642bed8b9aa473dc22d81fd92
Indunil Shashik <105118916+officialshashik@users.noreply.github.com>
2022-05-20 21:34:37 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,13 @@
+class Translation(object):
+
+      # Start text
+      START = "<b>ʜɪ ɪ ᴀᴍ ᴀʟ ᴋᴜᴘᴘɪʏᴀ ᴀᴜᴛᴏ ꜰᴏʀᴡᴀʀᴅᴇʀ 🐥💨.</b>\n<b>ᗩ/し ᏦᑌᑭᑭᏆᎩᗩᵀᴹ 🐣</b> \n<a href='https://t.me/apealkuppiya>ᴀʟᴇᴠᴇʟ ᴋᴜᴘᴘɪʏᴀ ɢʀᴏᴜᴘ 💁🏻‍♂️🎈</a> \n<a href='https://t.me/alevelkuppiya1>ᴀʟᴇᴠᴇʟ ᴋᴜᴘᴘɪʏᴀ ᴄʜᴀɴɴᴇʟ💁🏻‍♂️🎈</a>"             
+
+      #About text
+      ABOUT = """
+<b>📝 Language:</b> Python 3
+<b>🧰 Framework:</b> Pyrogram
+<b>👨‍💻 Developer:</b> ɪɴᴠɪꜱɪʙʟᴇ ᴇɪɴꜱᴛᴇɪɴ |ᴵᴱ ⚡️
+<b>📢 Channel:</b> <a href=<a href='https://t.me/alevelkuppiya1>ᴀʟᴇᴠᴇʟ ᴋᴜᴘᴘɪʏᴀ ᴄʜᴀɴɴᴇʟ💁🏻‍♂️🎈</a>
+<b>👥 Group:</b> <a href='https://t.me/apealkuppiya>ᴀʟᴇᴠᴇʟ ᴋᴜᴘᴘɪʏᴀ ɢʀᴏᴜᴘ 💁🏻‍♂️🎈</a>
+<b>🌐Source Code:</b> Press Me 🥰"""
